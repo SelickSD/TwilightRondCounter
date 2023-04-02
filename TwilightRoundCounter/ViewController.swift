1aea79959df00d4595044640123c5c0c21cbf8cd
sergey <SelickSD@Yandex.ru>
2023-04-02 22:43:38 +0300

    Initial commit

--- TwilightRoundCounter/ViewController.swift
+++ TwilightRoundCounter/ViewController.swift
@@ -9,11 +9,245 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var settingsButton: UIButton!
+    @IBOutlet var roundLabel: UILabel!
+
+    @IBOutlet var USSAView: UIView!
+    @IBOutlet var USAView: UIView!
+
+    private var currentRound = 1
+    private var currentAction = 1
+    private var isFirstAction = true
+
+    private var trailingUSAConstraint = NSLayoutConstraint()
+    private var leadingUSSAConstraint = NSLayoutConstraint()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        roundLabel.text = "Для запуска игры нажми кнопку Start"
+        setupGestures()
     }
 
+    private func setupGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapAction))
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(tapGesture)
+    }
 
+    private func startMiddleGame() {
+
+        let alert = UIAlertController(title: "Середина войны",
+                                      message: "В начале 4го хода в колоду замешиваются и раздаются карты середины войны\nОтбой в колоду не замешивается\nИгроки получают по 9 карт\nХод длится 7 рауднов",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Готово", style: .default, handler: nil)
+        alert.addAction(action)
+
+        DispatchQueue.main.async { () -> Void in
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    private func startEndGame() {
+
+        let alert = UIAlertController(title: "Окончание войны",
+                                      message: "В начале 8го хода в колоду замешиваются и раздаются карты Окончания войны\nОтбой в колоду не замешивается\nИгроки получают по 9 карт\nХод длится 7 рауднов",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Готово", style: .default, handler: nil)
+        alert.addAction(action)
+
+        DispatchQueue.main.async { () -> Void in
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    private func startNewAction() {
+        let alert = UIAlertController(title: "Новый ход: \(currentAction)",
+                                      message: "Обновите руку и сыграйте фазу заголовков",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ок", style: .default, handler: nil)
+        alert.addAction(action)
+
+        DispatchQueue.main.async { () -> Void in
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    private func setupView() {
+        trailingUSAConstraint = USAView.trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60)
+        leadingUSSAConstraint = USSAView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60)
+
+        trailingUSAConstraint.priority = UILayoutPriority(910)
+        leadingUSSAConstraint.priority = UILayoutPriority(910)
+
+        NSLayoutConstraint.activate([trailingUSAConstraint, leadingUSSAConstraint])
+        view.layoutIfNeeded()
+
+        roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+    }
+
+    @IBAction func startButtonClicked(_ sender: Any) {
+        startButton.isHidden = true
+
+        let alert = UIAlertController(title: "Начало войны",
+                                      message: "Раздайте каждому игроку по 8 карт.\nПоложите Карту Китая.\nПоместите дополнительное влияние\n СССР:\n6 единиц в Восточной Европе,\nСША: 7 единиц в Западной Европе\nХод длится 6 раундов",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Готово", style: .default, handler: {_ in
+            UIView.animate(withDuration: 0.5, delay: 0.0) {
+                self.setupView()
+            }
+        })
+        alert.addAction(action)
+
+        DispatchQueue.main.async { () -> Void in
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    private func USSAAction() {
+        trailingUSAConstraint = USAView.trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60)
+        leadingUSSAConstraint = USSAView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60)
+
+        trailingUSAConstraint.priority = UILayoutPriority(920)
+        leadingUSSAConstraint.priority = UILayoutPriority(930)
+
+        UIView.animate(withDuration: 0.5, delay: 0.0) {
+            NSLayoutConstraint.activate([self.trailingUSAConstraint, self.leadingUSSAConstraint])
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    private func USAAction() {
+        trailingUSAConstraint = USAView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60)
+        leadingUSSAConstraint = USSAView.leadingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60)
+
+        trailingUSAConstraint.priority = UILayoutPriority(920)
+        leadingUSSAConstraint.priority = UILayoutPriority(930)
+
+        UIView.animate(withDuration: 0.5, delay: 0.0) {
+            NSLayoutConstraint.activate([self.trailingUSAConstraint, self.leadingUSSAConstraint])
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    private func switchAction() {
+        switch currentAction {
+        case 1...3:
+            if currentRound < 6 {
+                if isFirstAction {
+                    isFirstAction.toggle()
+                    USAAction()
+                } else {
+                    isFirstAction.toggle()
+                    currentRound += 1
+                    roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+                    USSAAction()
+                }
+            } else {
+                if isFirstAction {
+                    isFirstAction.toggle()
+                    USAAction()
+                } else if currentAction == 3 {
+                    isFirstAction.toggle()
+                    currentRound = 1
+                    currentAction += 1
+                    roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+                    USSAAction()
+                    startMiddleGame()
+                } else {
+                    isFirstAction.toggle()
+                    currentRound = 1
+                    currentAction += 1
+                    roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+                    USSAAction()
+                    startNewAction()
+                }
+            }
+        case 4...7:
+            if currentRound < 7 {
+                if isFirstAction {
+                    isFirstAction.toggle()
+                    USAAction()
+
+                } else {
+                    isFirstAction.toggle()
+                    currentRound += 1
+                    roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+                    USSAAction()
+                }
+            } else {
+                if isFirstAction {
+                    isFirstAction.toggle()
+                    USAAction()
+                } else if currentAction == 7 {
+                    isFirstAction.toggle()
+                    currentRound = 1
+                    currentAction += 1
+                    roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+                    USSAAction()
+                    startEndGame()
+                } else {
+                    isFirstAction.toggle()
+                    currentRound = 1
+                    currentAction += 1
+                    roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+                    USSAAction()
+                    startNewAction()
+                }
+            }
+
+        case 10:
+            if currentRound < 7 {
+                if isFirstAction {
+                    isFirstAction.toggle()
+                    USAAction()
+
+                } else {
+                    isFirstAction.toggle()
+                    currentRound += 1
+                    roundLabel.text = "Ход: \(currentAction) Раунд: \(currentRound)"
+                    USSAAction()
+                }
+            } else {
+                if isFirstAction {
+                    isFirstAction.toggle()
+                    USSAAction()
+                } else {
+                    let alert = UIAlertController(title: "Конец Игры",
+                                                  message: "Выполните финальный подсчет очков",
+                                                  preferredStyle: .alert)
+                    let action = UIAlertAction(title: "Ок", style: .default, handler: nil)
+                    alert.addAction(action)
+
+                    DispatchQueue.main.async { () -> Void in
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+        default:
+            break
+        }
+    }
+
+    @IBAction func settingButtonTap(_ sender: Any) {
+        trailingUSAConstraint = USAView.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
+        leadingUSSAConstraint = USSAView.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
+
+        trailingUSAConstraint.priority = UILayoutPriority(900)
+        leadingUSSAConstraint.priority = UILayoutPriority(900)
+
+        NSLayoutConstraint.activate([trailingUSAConstraint, leadingUSSAConstraint])
+        view.layoutIfNeeded()
+
+        currentAction = 1
+        currentRound = 1
+
+        roundLabel.text = "Для запуска игры нажми кнопку Start"
+        startButton.isHidden = false
+    }
+
+    @objc private func tapAction() {
+        switchAction()
+    }
 }
 
