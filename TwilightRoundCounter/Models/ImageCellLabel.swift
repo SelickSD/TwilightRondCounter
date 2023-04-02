47cfc678cb48ff0b770cb2b1073b1b583995064e
sergey <SelickSD@Yandex.ru>
2023-04-02 23:40:53 +0300

    Добавил картинки и начал перерабатывать макет

---
+++ TwilightRoundCounter/Models/ImageCellLabel.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCellLabel.swift
+//  TwilightRoundCounter
+//
+//  Created by Сергей Денисенко on 02.04.2023.
+//
+
+import Foundation
+
+struct ImageCellLabel {
+    let imageName: String
+    let isOneRoundAvailable: Bool
+}
